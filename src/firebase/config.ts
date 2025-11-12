eac565cc55399409a580687f256d58ca6fd913ea
Akash Gupta <akashg6980@gmail.com>
2025-11-12 13:40:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7389894379-55fa1",
+  "appId": "1:978283798878:web:d6a20e835a907525292fcf",
+  "apiKey": "AIzaSyBD8KByu15AYFAHWarmjB3U8JLZvUflx6M",
+  "authDomain": "studio-7389894379-55fa1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "978283798878"
+};
